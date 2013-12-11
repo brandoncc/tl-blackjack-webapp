729f9bdd1c44633b3db2a46e6c93b9a7061a0e9b
Brandon Conway <brandoncc@gmail.com>
2013-12-11 14:21:08 -0800

    use @

--- classes/deck.rb
+++ classes/deck.rb
@@ -5,9 +5,11 @@ class Deck
 
   def initialize
     6.times do
+    @discard_pile   = []
+    @shuffled_cards = []
       %w(Clubs Diamonds Hearts Spades).each do |s|
         %w(2 3 4 5 6 7 8 9 10 Jack Queen King Ace).each do |v|
-          self.shuffled_cards << Card.new(v, s)
+          @shuffled_cards << Card.new(v, s)
         end
       end
     end
@@ -16,17 +18,17 @@ class Deck
   end
 
   def deal_one_card
-    shuffle_deck! if self.shuffled_cards.count == 0
-    self.shuffled_cards.shift
+    shuffle_deck! if @shuffled_cards.count == 0
+    @shuffled_cards.shift
   end
 
   def shuffle_deck!
     discard_pile.count.times do
-      self.shuffled_cards << self.discard_pile.pop
+      @shuffled_cards << @discard_pile.pop
     end
 
     15.times do
-      self.shuffled_cards.shuffle!
+      @shuffled_cards.shuffle!
     end
   end
 end
