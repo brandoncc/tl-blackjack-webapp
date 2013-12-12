11a4a7659032ead319a844a9e00c9d6340b3a656
Brandon Conway <brandoncc@gmail.com>
2013-12-11 19:42:43 -0800

    face_value and numeric_value

--- classes/card.rb
+++ classes/card.rb
@@ -9,4 +9,21 @@ class Card
   def image
     "/images/cards/#{suit.downcase}_#{value.downcase}.jpg"
   end
+
+  def face_value
+    "#{@value}"
+  end
+
+  def numeric_value
+    case @value.to_i
+    when 0
+      if @value == 'Ace'
+        11
+      else
+        10
+      end
+    else
+      @value.to_i
+    end
+  end
 end
