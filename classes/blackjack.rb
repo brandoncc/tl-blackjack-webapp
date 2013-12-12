1a66ff13051d7700e61a00023dc9e2821f87577b
Brandon Conway <brandoncc@gmail.com>
2013-12-12 11:51:16 -0800

    update status constants

--- classes/blackjack.rb
+++ classes/blackjack.rb
@@ -1,20 +1,19 @@
 class Blackjack
   attr_accessor :player, :dealer, :deck
 
-  INITIAL_CHIPS_VALUE  = 250
   INITIAL_CHIPS_VALUE     = 250
   DEALER_STAY_MINIMUM     = 17
   BLACKJACK_VALUE         = 21
 
   # win codes
-  PLAYER_HAS_BLACKJACK = 1001
-  PLAYER_HAS_21        = 1001
-  PLAYER_BUSTED        = 1002
-  DEALER_HAS_BLACKJACK = 2001
-  DEALER_HAS_21        = 2002
-  DEALER_BUSTED        = 2003
-  NO_WINNER_YET        = 3001
-
+  PLAYER_HAS_BLACKJACK    = 1001
+  PLAYER_BUSTED           = 1002
+  PLAYER_WINS             = 1003
+  DEALER_HAS_BLACKJACK    = 2001
+  DEALER_BUSTED           = 2003
+  DEALER_WINS             = 2005
+  NO_WINNER_YET           = 3001
+  GAME_IS_PUSH            = 3002
 
   def initialize
     @player      = Player.new
@@ -40,11 +39,11 @@ class Blackjack
 
   def game_status
     return PLAYER_HAS_BLACKJACK if @player.hand_is_blackjack?
-    return PLAYER_HAS_21 if @player.hand_is_blackjack? && @player.card_count > 2
     return PLAYER_BUSTED if @player.hand_is_bust?
+    return PLAYER_WINS if @player.won?
     return DEALER_HAS_BLACKJACK if @dealer.hand_is_blackjack?
-    return DEALER_HAS_21 if @dealer.hand_is_blackjack? && @dealer.card_count > 2
     return DEALER_BUSTED if @dealer.hand_is_bust?
+    return DEALER_WINS if @dealer.won?
     return NO_WINNER_YET
   end
 end
