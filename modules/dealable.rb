2ae9e11c38da9bb2f67e5e00921410300c0f8257
Brandon Conway <brandoncc@gmail.com>
2013-12-11 19:43:10 -0800

    calculate hand value

--- modules/dealable.rb
+++ modules/dealable.rb
@@ -4,6 +4,63 @@ module Dealable
   end
 
   def first_card
-    cards.first
+    @cards.first
+  end
+
+  def hand_is_blackjack?
+    @cards.count == 2 && value == 21
+  end
+
+  def hand_is_bust?
+    @value > 21
+  end
+
+  def hand_value
+    calculate_value[:value]
+  end
+
+  def hand_is_soft_or_hard
+    calculate_value[:soft_or_hard]
+  end
+
+  def calculate_value
+    score      = { value: 0, soft_or_hard: nil }
+    aces_count = 0
+
+    @cards.each do |card|
+      if card.numeric_value == 11
+        aces_count += 1
+      else
+        score[:value] += card.numeric_value.to_i
+      end
+    end
+
+    calculate_aces(score, aces_count)
+  end
+
+  # @param [Hash] current_score
+  # @param [Fixnum] count
+  def calculate_aces(current_score, count)
+    # If any items are in the ace array, calculate them in based on whether
+    #   they need to be 11 points or 1 point. 11 points is preferred, unless it
+    #   will cause a bust, in which case, calculate it as 1 point.
+    i = 0
+    while i < count
+      add_ace_to_score(current_score, i == 0)
+
+      i += 1
+    end
+    current_score[:soft_or_hard] = nil if current_score[:value] > 21
+    current_score
+  end
+
+  def add_ace_to_score(score, set_soft_or_hard)
+    if (score[:value] + 11) > 21
+      score[:value] += 1
+      score[:soft_or_hard] = 'hard' if set_soft_or_hard
+    else
+      score[:value] += 11
+      score[:soft_or_hard] = 'soft' if set_soft_or_hard
+    end
   end
 end
