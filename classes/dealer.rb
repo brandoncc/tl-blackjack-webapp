239fb84087975e28dd0b0c7c18a152e2fd21576b
Brandon Conway <brandoncc@gmail.com>
2013-12-12 16:43:22 -0800

    Dealer#card_showing

--- classes/dealer.rb
+++ classes/dealer.rb
@@ -12,4 +12,8 @@ class Dealer
   def in_stay_range?
     hand_value >= Blackjack::DEALER_STAY_MINIMUM && hand_value <= Blackjack::BLACKJACK_VALUE
   end
+
+  def card_showing
+    "showing #{cards.first}"
+  end
 end
